ee41b6ed206ecb668a853885a8d4f2d7ca94d423
NZRV88 <a.naserati@gmail.com>
2025-10-15 20:15:20 +0300

    Добавление заклинаний паладина повелителя драконов

--- client/data/spells/index.ts
+++ client/data/spells/index.ts
@@ -2,12 +2,18 @@
 import { Cantrips } from "./cantrips";
 import { SpellsLevel1 } from "./spellLevel1";
 import { SpellsLevel2 } from "./spellLevel2";
+import { SpellsLevel3 } from "./spellLevel3";
+import { SpellsLevel4 } from "./spellLevel4";
+import { SpellsLevel5 } from "./spellLevel5";
 // import { Level2Spells } from "./level2";
 // ...
 
 export const Spells = [
     ...Cantrips,
     ...SpellsLevel1,
-    ...SpellsLevel2
+    ...SpellsLevel2,
+    ...SpellsLevel3,
+    ...SpellsLevel4,
+    ...SpellsLevel5,
     // ...Level2Spells,
 ];
